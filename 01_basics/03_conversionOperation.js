d97cfad68d723d4ff101f06d26b56a7bab7ffbb0
shivam modi <159185532+Shivam2005modi@users.noreply.github.com>
2025-05-25 08:28:21 +0000

    conversion operation practice

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,50 @@
+// let score = 33
+let score1 = "33"
+
+console.log(typeof score1)
+console.log(typeof(score1));
+console.log(score1)
+
+let valueInNumber1 = Number(score1)
+console.log(typeof valueInNumber1);
+console.log(valueInNumber1);
+
+let score2 = "33a"
+
+console.log(typeof score2)
+console.log(typeof(score2));
+console.log(score2)
+
+let valueInNumber2 = Number(score2)
+console.log(typeof valueInNumber2);
+console.log(valueInNumber2);
+
+let score3 = "null"
+
+console.log(typeof score3)
+console.log(typeof(score3));
+console.log(score3)
+let valueInNumber3 = Number(score3)
+console.log(typeof valueInNumber3);
+console.log(valueInNumber3);
+
+let score4 = "undefined"
+
+console.log(typeof score4)
+console.log(typeof(score4));
+console.log(score4)
+
+let valueInNumber4 = Number(score4)
+console.log(typeof valueInNumber4);
+console.log(valueInNumber4);
+
+let score5 = "true"
+
+console.log(typeof score5)
+console.log(typeof(score5));
+console.log(score5)
+let valueInNumber5 = Number(score5)
+console.log(typeof valueInNumber5);
+console.log(valueInNumber5);
+
+
---
+++ 01_basics/03_conversionOperation2.js
@@ -0,0 +1,10 @@
+let isLoggedIn = ""
+let boolIsLoggedIn = Boolean(isLoggedIn)
+console.log(boolIsLoggedIn);
+
+
+let someNumber 
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
