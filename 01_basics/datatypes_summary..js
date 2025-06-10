373a95c5d1509c82a54970415dc8b435b6f5e23f
shivam modi <159185532+Shivam2005modi@users.noreply.github.com>
2025-06-10 10:36:38 +0000

    further learning

---
+++ 01_basics/datatypes_summary..js
@@ -0,0 +1,20 @@
+// js is dynamically typed language 
+
+// Primitive
+//=>call by value 
+//=> 7types  : String , Number , Boolean , null , undefined , Symbol , BigInt
+
+const score = 100
+const isloggedin =false 
+const outsidetemp = null
+let useremail
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id == anotherid);// false 
+const bigNumber = 1234567897879n 
+
+//non primitive
+//=>refrence type 
+//=> Array , Object , Funcions  
+
+
